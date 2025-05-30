64515d4148afd0c241c85cac337aeea358cfdeaf
chen <joe0917@163.com>
2025-05-30 14:34:37 +0800

    修改更新

--- source/conf.py
+++ source/conf.py
@@ -1,5 +1,5 @@
 project = '老子-道德经'
-copyright = '2025, Chen-jiangqiao'
+copyright = '2025, pyzpyjxh@163.com'
 author = '陈江桥'
 release = 'v1.0'
 
@@ -26,7 +26,9 @@ source_suffix = {
 # 图片路径设置
 html_static_path = ['_static']
 html_js_files = ['svg-interaction.js',]
+html_css_files = ['custom.css']
 
-#响应式SVG的CSS
-def setup(app):
-    app.add_css_file('custom.css')
+# 使用系统字体回退方案
+html_theme_options = {
+    'font_family': "'STSong', 'SimSun', serif",
+}
